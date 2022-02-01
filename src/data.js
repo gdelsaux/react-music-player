99fe8c7d5f4e25def10d23e7e8ee71df474ee99b
gdelsaux <gaetandelsaux@gmail.com>
2022-02-01 08:53:30 +0000

    added extra tracks to library

--- src/data.js
+++ src/data.js
@@ -51,6 +51,66 @@ function chillhop() {
       color: ['#9b7794', '#e9a7bb'],
       id: uuidv4(),
       active: false
+    },
+    {
+      name: "Hung Up",
+      cover:
+        "https://chillhop.com/wp-content/uploads/2021/09/dc49d7971512be0192848d072efdaaab91a4c73c-1024x1024.jpg",
+      artist: "Ezzy",
+      audio: 'https://mp3.chillhop.com/serve.php/?mp3=24699',
+      color: ['#d44848', '#f9dea8'],
+      id: uuidv4(),
+      active: false
+    },
+    {
+      name: "Higher",
+      cover:
+        "https://chillhop.com/wp-content/uploads/2021/05/2473c60e471fe9b40e246bf7711c87d3d6ea69a7-1024x1024.jpg",
+      artist: "Misha, Jussi Halme",
+      audio: 'https://mp3.chillhop.com/serve.php/?mp3=15088',
+      color: ['#3b4a83', '#f89d99'],
+      id: uuidv4(),
+      active: false
+    },
+    {
+      name: "Sunny in Saint Petersburg",
+      cover:
+        "https://chillhop.com/wp-content/uploads/2021/03/7945133f0b1f4f167f056fe0c369f05f2ef415f2-1024x1024.jpg",
+      artist: "Saib",
+      audio: 'https://mp3.chillhop.com/serve.php/?mp3=16076',
+      color: ['#d45c4d', '#f1e677'],
+      id: uuidv4(),
+      active: false
+    },
+    {
+      name: "Golden",
+      cover:
+        "https://chillhop.com/wp-content/uploads/2021/02/7f102bdde417f6ead9a120b2b931449e5c12b4da-1024x1024.jpg",
+      artist: "Aarigod",
+      audio: 'https://mp3.chillhop.com/serve.php/?mp3=13005',
+      color: ['#5f9122', '#cdeb8b'],
+      id: uuidv4(),
+      active: false
+    },
+    {
+      name: "Skates",
+      cover:
+        "https://chillhop.com/wp-content/uploads/2021/03/74d62bc9370a68e440c1b98eaf650344f0a7faea-1024x1024.jpg",
+      artist: "SwuM",
+      audio: 'https://mp3.chillhop.com/serve.php/?mp3=15226',
+      color: ['#4f503b', '#ac6b2c'],
+      id: uuidv4(),
+      active: false
+    },
+    {
+      name: "Esfera",
+      cover:
+        "https://chillhop.com/wp-content/uploads/2021/07/4163ebb931e06d4ee8eb184295c0246d4a5055f7-1024x1024.jpg",
+      artist: "Juan Rios, Djemeia",
+      audio: 'https://mp3.chillhop.com/serve.php/?mp3=21646',
+      color: ['#725186', '#f9bdb2'],
+      id: uuidv4(),
+      active: false
     }
   ];
 }
